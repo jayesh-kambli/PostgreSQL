ee9417aa6e1d68748292b909d01a3fc86bf4e9d4
Jayesh Kambli <jaykambli4465@gmail.com>
2026-04-15 19:22:09 +0530

    Day 10 partially completed (till-7)

--- batches/25/answers/Day10_answers.sql
+++ batches/25/answers/Day10_answers.sql
@@ -67,6 +67,59 @@ GROUP BY p.product_id, p.product_name,
 	END
 ORDER BY p.product_id
 
+-- 5)
+	-- Method 1 (every order has multiple entries)
+select o.order_id, c.first_name, p.product_name, oi.quantity, oi.net_amount, s.store_name, coalesce(pay.payment_mode, 'Not Paid') as payment_mode  from sales.orders o
+	LEFT JOIN customers.customers c ON c.customer_id = o.cust_id
+	LEFT JOIN sales.order_items oi USING(order_id)
+	LEFT JOIN products.products p ON oi.prod_id = p.product_id 
+	LEFT JOIN stores.stores s USING(store_id)
+	LEFT JOIN sales.payments pay using(order_id)
+ORDER BY o.order_id
+
+	-- Method 2 (every order as single entry [product names are aggregated and listed using STRING_AGG])
+select o.order_id, c.first_name, ord_prod.product_name, ord_prod.total_quantity, o.net_total, s.store_name, coalesce(pay.payment_mode, 'Not Paid') as payment_mode from sales.orders o
+	LEFT JOIN customers.customers c ON c.customer_id = o.cust_id
+	LEFT JOIN (
+		select 
+			order_id, count(quantity) as total_quantity, 
+			STRING_AGG(p.product_name, ', ') as product_name 
+		from sales.order_items oi
+		LEFT JOIN products.products p ON p.product_id = oi.prod_id
+		GROUP BY order_id
+	) ord_prod USING(order_id)
+	LEFT JOIN stores.stores s USING(store_id)
+	LEFT JOIN sales.payments pay using(order_id)
+ORDER BY o.order_id
+
+-- 6)
+select  b.brand_id, b.brand_name, count(DISTINCT p.product_id) as product_count, ROUND(AVG(p.price), 2) as avg_price, SUM(oi.quantity) as total_quantity_sold, SUM(oi.net_amount) as total_revenue from core.dim_brand b
+	LEFT JOIN products.products p USING(brand_id) 
+	LEFT JOIN sales.order_items oi ON oi.prod_id = p.product_id
+	GROUP BY b.brand_id, b.brand_name
+ORDER BY brand_id
+
+-- 7) 
+select s.store_name, r.region_name, e.total_employee_count, o.total_orders, o.total_revenue from stores.stores s
+	LEFT JOIN core.dim_region r USING(region_id)
+	LEFT JOIN (
+		select store_id, count(employee_id) as total_employee_count from stores.employees GROUP BY store_id
+	) e USING(store_id)
+	LEFT JOIN (
+		select store_id, count(order_id) as total_orders, SUM(net_total) as total_revenue from sales.orders GROUP BY store_id
+	) o USING(store_id)
+
+
+select * from core.dim_brand
+select * from products.products
+select * from sales.order_items
+
+select * from customers.customers
+select * from sales.order_items
+select * from products.products
+select * from stores.stores
+select * from sales.payments
+select * from stores.employees
 
 select * from sales.orders
 select * from sales.shipments
