3136db8c58247322612711c634df431e1b2bf542
Jayesh Kambli <jaykambli4465@gmail.com>
2026-04-22 15:25:35 +0530

    Day 16 completed till what was taught (till-Q4)

---
+++ batches/25/answers/Day16_answers.sql
@@ -0,0 +1,32 @@
+-- 1)
+select 
+	*,
+	ROUND(budget * 100.0 / (sum(budget) OVER()),3) as pct
+from marketing.campaigns
+
+-- 2)
+select 
+	*,
+	ROUND(AVG(salary) OVER(partition by store_id),2) as store_avg,
+	salary  - ROUND(AVG(salary) OVER(partition by store_id),2) as diff
+from stores.employees
+
+-- 3)
+select 
+	*,
+	count(price) OVER(partition by brand_id) as total_prod_in_brand,
+	MIN(price) OVER(partition by brand_id) as min_price_in_brand,
+	MAX(price) OVER(partition by brand_id) as max_price_in_brand
+from products.products
+
+-- 4)
+select 
+	*,
+	round(avg(net_total) OVER(partition by store_id),2) as store_avg,
+	CASE
+		WHEN net_total > (round(avg(net_total) OVER(partition by store_id),2)*3) THEN 'High Value'
+		ELSE ' '
+	END
+from sales.orders
+
+
